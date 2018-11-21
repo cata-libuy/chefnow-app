2be5851bb12cbba1e8c263e03e21f52db5afcfba
josedecelis <jose.de@sansano.usm.cl>
2018-11-20 21:33:56 -0300

    Agregamos botón guardar receta

--- src/pages/CrearReceta.vue
+++ src/pages/CrearReceta.vue
@@ -3,13 +3,14 @@
 
       
       
-<q-page padding class="docs-input row justify-center">
+<q-page padding class="">
     Crear Receta
-    <q-input class="" float-label="Título de receta" type="text"/>
-    <q-input class="" float-label="Escribe tu receta" type="text-area" rows="5"/>
-    <q-input class="" suffix="Horas" float-label="tiempo de duración" type="number" />
-    <q-input class="" prefix="Minutos" float-label="tiempo de duración" type="number" />
-    <q-input class="" value="0" stack-label="Porciones" />
+    <q-input class="row" float-label="Título de receta" type="text"/>
+    <q-input class="row" float-label="Escribe tu receta" type="text-area" rows="5"/>
+    <q-input class="row" suffix="Horas" float-label="tiempo de duración" type="number" />
+    <q-input class="row" prefix="Minutos" float-label="tiempo de duración" type="number" />
+    <q-input class="row" value="0" stack-label="Porciones" />
+    <q-btn  icon = "create"  label = "Guardar Receta" color="primary"/>
   </q-page>
 </template>
 
